51936e4377626a3dba99f85de1a5f211c9fdd37d
Gerdinebehr <gerdine672@revature.net>
2024-09-11 14:17:36 -0400

    Exit funstionality started, not funtional

--- exercises/week_1/GST.js
+++ exercises/week_1/GST.js
@@ -12,7 +12,6 @@ let groceryList = []; //let because the value can change as we add items , [] fo
 // Function list for users 
 /* users need to be able to 
 
-
 -ADD 
     -Item names
     -Quantities 
@@ -22,32 +21,50 @@ let groceryList = []; //let because the value can change as we add items , [] fo
     -Set whether existing item has been bought 
 
     */
-
-
     const listOptions = () => { //arrow function to display options
       console.log("\nO p t i o n s :");
-      console.log("Add New Item");
-      console.log("Remove Item");
-      console.log("View List");
+      console.log("Add");
+      console.log("Remove");
+      console.log("View");
     } ;
 
+    //call function to display options 
 
+    listOptions();
 
+    //function to handle user input 
 
+    const processOption = (line) => { //stores option to be used
 
+      //checks if user input is Add New Item
 
+      if(line.startsWith('Add ')){
+        const item = line.slice(4);
+        groceryList.push(item); // adds each item to grocery list
+      }
+      else if(line === View){
+        groceryList.forEach((item, index) => {
+          console.log(`${index + 1} , ${item}`);
+        })
+      }
+      else if(line === "Exit"){
+        rl.close();
+      }
+      else {
+        console.log("No Item Found");}
+  };
 
-
-
-
-
-
-
+   //Readline interface set up that was given to you (MUST EDIT)
 
 rl.on('line', (line) => {
     console.log(line);
 });
 
+rl.on('line', (line) => {
+  processOption(line); //process the input line
+});
+
+// Handle when the readline interface is closed 
 rl.once('close', () => {
      // end of input
      console.log("Goodbye");
